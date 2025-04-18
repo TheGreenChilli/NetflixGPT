94ffdf333e3380bccbb5f914f7863f217d531d4b
sanjana <sanju.kri2112@gmail.com>
2025-04-19 00:21:52 +0530

    Add movie-related components and hooks; implement movie fetching and display logic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,3 +12,5 @@ export const API_OPTIONS = {
       "Bearer " + process.env.REACT_APP_TMDB_KEY,
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/"
--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -4,10 +4,20 @@ const moviesSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
+    trailerVideo: null,
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
+    },
+    addPopularMovies: (state, action) => {
+      state.popularMovies = action.payload;
+    },
+    addTrendingMovies: (state, action) => {
+      state.trendingMovies = action.payload;
+    },
+    addUpcomingMovies: (state, action) => {
+      state.upcomingMovies = action.payload;
     },
     addTrailerVideo: (state, action) => {
       state.trailerVideo = action.payload;
@@ -15,6 +25,6 @@ const moviesSlice = createSlice({
   },
 });
 
-export const { addNowPlayingMovies, addTrailerVideo } = moviesSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo, addPopularMovies, addTrendingMovies, addUpcomingMovies} = moviesSlice.actions;
 
 export default moviesSlice.reducer;
