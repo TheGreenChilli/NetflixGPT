cb68bfad89c3978bbe058678c7681f9260132d5c
sanjana <sanju.kri2112@gmail.com>
2025-04-24 01:23:14 +0530

    Update README.md to include OpenAI API integration details and site responsiveness improvements

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,19 +1,20 @@
+import { BG_URL } from "../utils/constants";
 import GptMovieSuggestions from "./GptMovieSuggestions";
 import GptSearchBar from "./GptSearchBar";
-import { BG_URL } from "../utils/constants";
 
 const GptSearch = () => {
   return (
-    <div>
-      <div className="fixed -z-10">
-        <img
-          src={BG_URL}
-          alt="logo"
-        />
+    <>
+      <div>
+        <div className="fixed -z-10">
+          <img className="h-screen object-cover" src={BG_URL} alt="logo" />
+        </div>
+        <div className="pt-[30%] md:p-0">
+          <GptSearchBar />
+          <GptMovieSuggestions />
+        </div>
       </div>
-      <GptSearchBar />
-      <GptMovieSuggestions />
-    </div>
+    </>
   );
 };
 
